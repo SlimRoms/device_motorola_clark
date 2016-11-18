63611d112942c5cfa7a8f8109b9b892345929904
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 03:36:00 -0600

    clark: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8

--- slim.mk
+++ slim.mk
@@ -7,9 +7,6 @@ $(call inherit-product, device/motorola/clark/device.mk)
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
-
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
